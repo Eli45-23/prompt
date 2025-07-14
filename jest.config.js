3de6b8cda6548757faa81e32835453f991f3a99d
Eli <eli@mac.myfiosgateway.com>
2025-07-13 20:25:09 -0400

    Configure Jest with ts-jest for TypeScript support

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)
+: '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\.(ts|tsx)
+: 'ts-jest',
   },
 };
