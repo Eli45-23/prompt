afc963d67a9e9c5dde5bb73199080030e522b396
Eli <eli@mac.myfiosgateway.com>
2025-07-13 20:32:14 -0400

    Fix corrupted jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,11 @@
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)
+    '^@/(.*)$' : '<rootDir>/src/$1',
   },
   transform: {
-    '^.+\.(ts|tsx)
-: 'ts-jest',
-  },
-};
- : '<rootDir>/src/$1',
-  },
-  transform: {
-    '^.+\.(ts|tsx)
-: 'ts-jest',
+    '^.+\.(ts|tsx)$' : 'ts-jest',
   },
 };
